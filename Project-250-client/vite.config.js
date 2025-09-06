a7d0c7fd7b314bbc1fff98cac4d67b4bcb041d75
Akul85 <biswasakul287@gmail.com>
2025-09-06 15:45:51 +0600

    config fixed

--- Project-250-client/vite.config.js
+++ Project-250-client/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+    allowedHosts: ['cse-250-client-3.onrender.com'] // allow Render domain
+  }
 })
